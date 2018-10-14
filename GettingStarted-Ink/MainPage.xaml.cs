d31243e3cb7b137c1308b4a5587359f4c5b28d60
Tyrell Collins <ElectricMantis88@outlook.com>
2018-10-14 09:11:29 -0700

    step2

--- GettingStarted-Ink/MainPage.xaml.cs
+++ GettingStarted-Ink/MainPage.xaml.cs
@@ -22,18 +22,18 @@
 //  THE SOFTWARE.
 //  ---------------------------------------------------------------------------------
 
-using System;
-using System.Collections.Generic;
+//using System;
+//using System.Collections.Generic;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Media;
 
 // Begin "Step 2: Use InkCanvas to support basic inking"
 ////using directives for inking functionality.
-//using Windows.UI.Input.Inking;
-//using Windows.UI.Input.Inking.Analysis;
-//using Windows.UI.Xaml.Shapes;
-//using Windows.Storage.Streams;
+using Windows.UI.Input.Inking;
+using Windows.UI.Input.Inking.Analysis;
+using Windows.UI.Xaml.Shapes;
+using Windows.Storage.Streams;
 // End "Step 2: Use InkCanvas to support basic inking"
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
